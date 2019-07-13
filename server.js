1ec06ba73d5f2cf1d21c5158a1dd239323d84d8c
nero <awabxnero@gmail.com>
2019-07-13 21:49:29 +0800

    commit

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use("/api/items", items);
 
 //serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("/client/build"));
+  // Set static folder
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
